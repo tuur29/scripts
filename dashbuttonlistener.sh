8688d27f832690ff91cd8e0a1bab0d5b94a27d30
Tuur Lievens <tuur.lievens@hotmail.com>
2018-05-29 15:40:07 +0200

    Update dashbuttonlistener
    - Fix not reacting on first dashbutton press after long delay
    - Improve documentation

--- dashbuttonlistener.sh
+++ dashbuttonlistener.sh
@@ -6,12 +6,13 @@
 # 2. Lookup it's MAC address in you routes DHCP client list
 # 3. Deny all traffic from that MAC address in your routers firewall
 # 4. Change the MAC address and the function below
-# 5. Move this file to a local server (like a raspberry pi)
-# 6. Add the following cronjob (crontab -e) to ensure it's always running:
+# 5. Check if your sever is connected to the network on 'eth0' or change accordingly
+# 6. Move this file to a local server (like a raspberry pi)
+# 7. Add the following cronjob (crontab -e) to ensure it's always running:
 #    @reboot /home/user/dashbuttonlistener.sh > /dev/null 2>&1
 #
 
-sudo tcpdump 'arp' -e -i eth0 -n -p -t -l | while read line; do
+sudo tcpdump ether "dst host FF:FF:FF:FF:FF:FF and (port 67 or port 68)" -e -i eth0 -n -p -t -l | while read line; do
 
     if [[ "$line" =~ ^ab:cd:ef:gh:ij:kl ]]; then
 
